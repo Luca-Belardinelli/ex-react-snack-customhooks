695cc853d3226cda992705d1108f76e42d945fad
Luca Belardinelli <belardinelliluca@gmail.com>
2025-05-20 16:30:19 +0200

    Implement useCustomPointer hook and update App component to use custom cursor

--- src/App.jsx
+++ src/App.jsx
@@ -17,20 +17,36 @@
 // export default App;
 
 
-
-
-
 // SECONDO SNACK
 
-import useDate from "./components/useDate";
+// import useDate from "./components/useDate";
+
+// function App() {
+//   const currentDate = useDate();
+
+//   return (
+//     <div>
+//       <h1>Data e ora attuali:</h1>
+//       <p>{currentDate.toLocaleString()}</p>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+// TERZO SNACK
+
+import useCustomPointer from "./components/useCustomPointer";
 
 function App() {
-  const currentDate = useDate();
+  const customPointer = useCustomPointer("🔥");
 
   return (
     <div>
-      <h1>Data e ora attuali:</h1>
-      <p>{currentDate.toLocaleString()}</p>
+      <h1>Sposta il mouse per vedere il cursore personalizzato!</h1>
+      {customPointer}
     </div>
   );
 }
---
+++ src/components/useCustomPointer.JSX
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+function useCustomPointer(component) {
+    const [position, setPosition] = useState({ x: 0, y: 0 })
+    useEffect(() => {
+        const mouseMove = event => {
+            setPosition({ x: event.clientX, y: event.clientY })
+        }
+        document.addEventListener("mousemove", mouseMove)
+        return () => {
+            document.removeEventListener("mousemove", mouseMove);
+        }
+    }, []);
+    return (
+        <div style={{
+            position: "fixed",
+            top: position.y,
+            left: position.x,
+            transform: "translate(-50%, 50%)",
+            cursor: "none"
+        }}>
+            {component}
+        </div>
+    )
+}
+
+export default useCustomPointer;
